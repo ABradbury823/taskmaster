4c7363f25dcef829f052d7cb010c298291e5929a
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-10-03 18:30:46 -0400

    removed dangerous create table test

--- tests/db/test_postgresql.py
+++ tests/db/test_postgresql.py
@@ -7,10 +7,5 @@ class TestPostgreSQL(unittest.TestCase):
         result = exec_get_one('SELECT VERSION()')
         self.assertTrue(result[0].startswith('PostgreSQL'))
 
-    def test_create_table(self):
-        exec_commit("""CREATE TABLE example_table(
-        id SERIAL PRIMARY KEY,
-        foo TEXT NOT NULL);""")
-
 if __name__ == '__main__':
     unittest.main()
