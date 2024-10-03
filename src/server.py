0a3257d62dd39d69a92cff790c49886e588fd82f
Christian Ashley <christianzashley@gmail.com>
2024-10-03 15:21:59 -0400

    Add initial resource on server
    * Init src/api/root.py

--- src/server.py
+++ src/server.py
@@ -1,8 +1,12 @@
 from flask import *
 from flask_restful import Api
 
+from api.root import Root
+
 app = Flask(__name__)
 api = Api(app)
+
+api.add_resource(Root, '/')
 
 if __name__ == '__main__':
   app.run(host='::', port=4500, debug=True)
