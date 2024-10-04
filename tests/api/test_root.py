9c1f8477f3dc61d15367c3b7fc120e3e5a2cdc01
Christian Ashley <christianzashley@gmail.com>
2024-10-03 21:11:52 -0400

    Add initial test API request  utilities
    * Init tests/api/test_req_utils.py

--- tests/api/test_root.py
+++ tests/api/test_root.py
@@ -1,30 +1,26 @@
 import unittest
-import requests
+
+from .test_req_utils import test_get, test_post, test_put, test_delete
 
 base_url = 'http://localhost:4500'
 
 class TestRoot(unittest.TestCase):
   def test_get_returns_hello_world(self):
     """Root endpoint should return string 'Hello world!'"""
-    res = requests.get(base_url)
-    res_text = res.json() # parse json response
-    self.assertEqual(res.status_code, 200, 'Expected status code 200')
-    self.assertEqual(res_text, 'Hello world!', 'Expected text response of Hello World')
+    content = test_get(self, base_url)
+    self.assertEqual(content, 'Hello world!', 'Expected text response of Hello World')
 
   def test_post_returns_405_error(self):
     """Post requests are not allowed at root endpoint"""
-    res = requests.post(base_url)
-    self.assertEqual(res.status_code, 405, 'Expected status code 405')
+    test_post(self, base_url, expected_status=405)
 
   def test_put_returns_405_error(self):
     """Put requests are not allowed at root endpoint"""
-    res = requests.put(base_url)
-    self.assertEqual(res.status_code, 405, 'Expected status code 405')
+    test_put(self, base_url, expected_status=405)
 
   def test_delete_returns_405_error(self):
     """Delete requests are not allowed at root endpoint"""
-    res = requests.delete(base_url)
-    self.assertEqual(res.status_code, 405, 'Expected status code 405')
+    test_delete(self, base_url, expected_status=405)
 
 if __name__ == '__main__':
   unittest.main()
