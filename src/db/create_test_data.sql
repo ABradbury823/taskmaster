0b4ebf79b04c0b5c44f86ccbc5813e18a288deab
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-10-07 15:08:50 -0400

    removed admin tables in favor of bool on members

--- src/db/create_test_data.sql
+++ src/db/create_test_data.sql
@@ -3,13 +3,11 @@ DROP TABLE IF EXISTS test.task_categories CASCADE;
 DROP TABLE IF EXISTS test.categories CASCADE;
 DROP TABLE IF EXISTS test.tasks CASCADE;
 DROP TABLE IF EXISTS test.taskboards CASCADE;
-DROP TABLE IF EXISTS test.team_admins CASCADE;
 DROP TABLE IF EXISTS test.team_members CASCADE;
 DROP TABLE IF EXISTS test.teams CASCADE;
-DROP TABLE IF EXISTS test.admins CASCADE;
 DROP TABLE IF EXISTS test.users CASCADE;
 DROP SCHEMA IF EXISTS test CASCADE;
-
+--COMMIT;
 -- create schema and intialize parameters
 CREATE SCHEMA IF NOT EXISTS test;
 
@@ -30,12 +28,6 @@ CREATE TABLE IF NOT EXISTS test.users (
 	bio TEXT NOT NULL DEFAULT ''
 );
 
--- make admins table, a subclass of users
-CREATE TABLE IF NOT EXISTS test.admins (
-	id SERIAL PRIMARY KEY,
-	user_id INTEGER NOT NULL REFERENCES test.users(id)
-);
-
 -- make teams table
 CREATE TABLE IF NOT EXISTS test.teams (
 	id SERIAL PRIMARY KEY,
@@ -48,15 +40,8 @@ CREATE TABLE IF NOT EXISTS test.team_members (
 	id SERIAL PRIMARY KEY,
 	team_id INTEGER NOT NULL REFERENCES test.teams(id),
 	user_id INTEGER NOT NULL REFERENCES test.users(id),
-	joined_on DATE NOT NULL DEFAULT '-infinity'
-);
-
--- make team admins table, a join table between admins and teams
-CREATE TABLE IF NOT EXISTS test.team_admins (
-	id SERIAL PRIMARY KEY,
-	team_id INTEGER NOT NULL REFERENCES test.teams(id),
-	admin_id INTEGER NOT NULL REFERENCES test.admins(id),
-	joined_on DATE NOT NULL DEFAULT '-infinity'
+	joined_on DATE NOT NULL DEFAULT '-infinity',
+	is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- make taskboards table
@@ -97,27 +82,17 @@ INSERT INTO test.users (name, email, password) VALUES ('Adam', 'adam@email.com',
 INSERT INTO test.users (name, email, password) VALUES ('Eve', 'eve@email.com', 'eve<3apples');
 INSERT INTO test.users (name, email, password, display_name) VALUES ('Satan', 'notevil@email.com', 'evilgenius666', 'Serpent');
 
--- make test admins
-INSERT INTO test.admins (user_id) VALUES (1);
-INSERT INTO test.admins (user_id) VALUES (4);
-
 -- make test teams
 INSERT INTO test.teams (name) VALUES ('Eden Crew');
 INSERT INTO test.teams (name) VALUES ('Divine Realm');
 
 -- make test team members
-INSERT INTO test.team_members (team_id, user_id) VALUES (1, 1);
+INSERT INTO test.team_members (team_id, user_id, is_admin) VALUES (1, 1, TRUE);
 INSERT INTO test.team_members (team_id, user_id) VALUES (1, 2);
 INSERT INTO test.team_members (team_id, user_id) VALUES (1, 3);
-INSERT INTO test.team_members (team_id, user_id) VALUES (1, 4);
-INSERT INTO test.team_members (team_id, user_id) VALUES (2, 1);
-INSERT INTO test.team_members (team_id, user_id) VALUES (2, 4);
-
--- make test team admins
-INSERT INTO test.team_admins (team_id, admin_id) VALUES (1, 1);
-INSERT INTO test.team_admins (team_id, admin_id) VALUES (1, 2);
-INSERT INTO test.team_admins (team_id, admin_id) VALUES (2, 1);
-INSERT INTO test.team_admins (team_id, admin_id) VALUES (2, 2);
+INSERT INTO test.team_members (team_id, user_id, is_admin) VALUES (1, 4, TRUE);
+INSERT INTO test.team_members (team_id, user_id, is_admin) VALUES (2, 1, TRUE);
+INSERT INTO test.team_members (team_id, user_id, is_admin) VALUES (2, 4, TRUE);
 
 -- make test taskboards
 INSERT INTO test.taskboards (team_id, name) VALUES (1, 'Garden of Eden Tending');
