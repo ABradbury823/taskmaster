0c91fb7d344dca8a83a9fa749881a3b461f86f46
Christian Ashley <christianzashley@gmail.com>
2024-10-30 19:26:27 -0400

    Begin adding WHERE clause to Table.select

--- src/db/table.py
+++ src/db/table.py
@@ -10,16 +10,26 @@ class Table():
 
   def select(self, fields:list=[], where={}):
     filtered_fields = []
+    filtered_where = []
+    values = []
     
     for column in self._columns:
       if column['column_name'] in fields:
         filtered_fields.append(column['column_name'])
+      if column['column_name'] in where:
+        filtered_where.append(column['column_name'] + '=%s')
+        # TODO: validate type
+        values.append(where[column['column_name']])
 
     query = f"""
       SELECT {
         '*' if len(filtered_fields) == 0
         else ', '.join(filtered_fields)
       }
-      FROM {self._name};
+      FROM {self._name}
+      {
+        '' if len(filtered_where) == 0
+        else 'WHERE ' + ' AND '.join(filtered_where)
+       };
     """
-    return self._database.select(query)
+    return self._database.select(query, values)
