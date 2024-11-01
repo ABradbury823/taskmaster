fa4529f47c24642f151f73e6ba1ed9a0e97bd9ed
Christian Ashley <christianzashley@gmail.com>
2024-11-01 11:42:50 -0400

    Add insert method to Table

--- src/db/table.py
+++ src/db/table.py
@@ -1,14 +1,14 @@
 class Table():
   def __init__(
-      self, name: str, columns: list, 
-      # Typing this accurately results in circular import
-      database: 'Database' # type: ignore
+    self, name: str, columns: list, 
+    # Typing this accurately results in circular import
+    database: 'Database' # type: ignore
   ):
     self._name = name
     self._columns = columns
     self._database = database
 
-  def select(self, fields:list=[], where={}):
+  def select(self, fields:list=[], where:dict={}):
     filtered_fields = []
     filtered_where = []
     values = []
@@ -33,3 +33,29 @@ class Table():
        };
     """
     return self._database.select(query, values)
+  
+  def insert(self, fields:dict={}, returning:list=''):
+    filtered_fields = []
+    value_holder = []
+    filtered_returning = []
+    values = []
+
+    for column in self._columns:
+      if column['column_name'] in fields:
+        filtered_fields.append(column['column_name'])
+        value_holder.append('%s');
+        values.append(fields[column['column_name']])
+      if type(returning) is list and column['column_name'] in returning:
+        filtered_returning.append(column['column_name'])
+
+    query = f"""
+      INSERT INTO {self._name}
+      ({', '.join(filtered_fields)})
+      VALUES ({', '.join(value_holder)})
+      {
+        'RETURNING ' + 
+        ', '.join(filtered_returning) if type(returning) is list
+        else returning
+      };
+    """
+    return self._database.exec_commit(query, values)
