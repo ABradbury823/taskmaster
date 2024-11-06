353c65037a0d6faaeef59a36ab81283f9b5e7f6d
Christian Ashley <christianzashley@gmail.com>
2024-11-05 20:57:39 -0500

    Define Categories resource and endpoint

--- src/server.py
+++ src/server.py
@@ -5,6 +5,7 @@ from api.root import Root
 from api.tasks import Tasks
 from api.taskboards import Taskboards
 from api.taskboard import Taskboard
+from api.categories import Categories
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,6 +14,7 @@ api.add_resource(Root, '/')
 api.add_resource(Tasks, '/tasks')
 api.add_resource(Taskboards, '/taskboards')
 api.add_resource(Taskboard, '/taskboards/<int:id>')
+api.add_resource(Categories, '/categories')
 
 if __name__ == '__main__':
   app.run(host='::', port=4500, debug=True)
