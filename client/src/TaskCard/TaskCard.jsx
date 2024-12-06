35f9d75f619093985fec5840c6994eb39aaa6a71
Christian Ashley <christianzashley@gmail.com>
2024-12-05 19:16:29 -0500

    Initial TaskCard layout

--- client/src/TaskCard/TaskCard.jsx
+++ client/src/TaskCard/TaskCard.jsx
@@ -1,15 +1,32 @@
-import { Card, CardTitle, CardBody } from "reactstrap";
+import { Card, CardTitle, CardBody, Row, Col, Container } from "reactstrap";
 
 export default function TaskCard ({ task }) {
-  const { name, description } = task;
+  const { name, description, due_date, assignee_id } = task;
   return (
-    <Card>
-      <CardTitle tag='h4'>
-        {name}
-      </CardTitle>
-      <CardBody>
-        {description}
-      </CardBody>
-    </Card>
+    <Col xs={{ size: 10, offset: 1}} sm={{ size: 3, offset: 0}}>
+      <Card style={{ maxWidth: '18rem', minWidth: 'fit-content' }} >
+        <CardTitle style={{ 
+          padding: '0.5rem',
+          borderBottom: '0.05rem solid black'
+        }} tag='h4'>
+          {name}
+        </CardTitle>
+        <CardBody className="p-4 pt-0">
+          <Row>
+            <Col xs="6">
+              {assignee_id}
+            </Col>
+            <Col xs="6">
+              {due_date}
+            </Col>
+          </Row>
+          <Row style={{ textAlign: "left"}}>
+            <Col xs="12">
+              {description}
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+    </Col>
   );
 }
