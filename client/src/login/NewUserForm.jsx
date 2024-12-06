725c8db10e19265a88482c895940dc993b195760
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-12-05 20:52:47 -0500

    new user form ui

--- client/src/login/NewUserForm.jsx
+++ client/src/login/NewUserForm.jsx
@@ -1,23 +1,114 @@
 import {useState} from "react";
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { Button, Form, FormGroup, FormText, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
 export default function NewUserForm({open, onSubmit, onToggle}) {
+  const [username, setUsername] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const updateUsername = e => setUsername(e.target.value);
+  const updateDisplayName = e => setDisplayName(e.target.value);
+
+  function resetValues() {
+    setUsername("");
+    setDisplayName("");
+  }
+
+  function handleToggle() {
+    resetValues();
+    onToggle();
+  }
+
+  // TODO: validate data
+  function handleSubmit() {
+    onSubmit();
+    
+    resetValues();
+  }
+
   return (
-    <Modal isOpen={open} toggle={onToggle} centered> 
-      <ModalHeader toggle={onToggle}>New User Register</ModalHeader>
+    <Modal isOpen={open} toggle={handleToggle} centered> 
+      <ModalHeader toggle={handleToggle}>TaskMaster Sign Up</ModalHeader>
       <ModalBody>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Assumenda, id consequatur et ipsum ad corporis unde minus a placeat rem ea nulla. 
-        In porro, obcaecati at et eveniet minus doloribus repellat fugiat suscipit quis deserunt 
-        aspernatur assumenda, fugit similique. Numquam commodi enim quasi ipsum ullam. Iste, 
-        dicta assumenda. Ducimus, incidunt.
+        <Form>
+          <FormGroup floating>
+            <Input
+              id="new-username"
+              name="new-username"
+              placeholder="Username"
+              type="text"
+              onChange={updateUsername}
+              required
+            />
+            <Label for="new-username">Username</Label>
+          </FormGroup>
+          <FormGroup floating>
+            <Input
+              id="new-email"
+              name="new-email"
+              placeholder="Email"
+              type="email"
+              required
+            />
+            <Label for="new-email">Email</Label>
+          </FormGroup>
+          <FormGroup floating>
+            <Input
+              id="new-password"
+              name="new-password"
+              placeholder="Password"
+              type="password"
+              required
+            />
+            <Label for="new-password">Password</Label>
+          </FormGroup>
+          <FormGroup floating>
+            <Input
+              id="retype-password"
+              name="retype-password"
+              placeholder="Re-type Password"
+              type="password"
+              required
+            />
+            <Label for="retype-password">Re-Type Password</Label>
+          </FormGroup>
+          <FormGroup floating>
+            <Input
+              id="new-display-name"
+              name="new-display-name"
+              placeholder="Display Name (Optional)"
+              type="text"
+              onChange={updateDisplayName}
+            />
+            <Label for="new-display-name">Display Name (Optional)</Label>
+            <FormText>
+              This is the name other people will see you as:{" "}
+              {
+                <FormText color="success">
+                  {displayName !== "" ? displayName : username}
+                </FormText>
+              }
+            </FormText>
+          </FormGroup>
+          <FormGroup floating>
+            <Input
+              id="new-bio"
+              name="new-bio"
+              placeholder="About me..."
+              type="text"
+            />
+            <Label for="new-bio">About Me (Optional)</Label>
+            <FormText>
+              Tell us a little bit about yourself!
+            </FormText>
+          </FormGroup>
+        </Form>
       </ModalBody>
       <ModalFooter>
-        <Button color="success" onClick={onSubmit}>
+        <Button type="submit" onClick={handleSubmit}>
           Submit
         </Button>
         {" "}
-        <Button color="secondary" onClick={onToggle}>
+        <Button color="secondary" onClick={handleToggle}>
           Cancel
         </Button>
       </ModalFooter>
