73cd8e7e260631bdde07fff769165d02be7cd07c
Christian Ashley <christianzashley@gmail.com>
2024-12-10 11:23:17 -0500

    Clean up responsive Header in MainLayout

--- client/src/MainLayout/MainLayout.jsx
+++ client/src/MainLayout/MainLayout.jsx
@@ -1,28 +1,36 @@
 // TODO: Consider using state and react-router-dom useLocation to show active tab
-// import React, { useState } from 'react';
 import { useState, useEffect, useRef } from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router';
 import { AuthContext } from '../Context';
 
 function MainLayout() {
-  const [headerOffset, setHeaderOffset] = useState('0px');
+  // use MainLayout to keep track of signed in user,
+  // will provide context to rest of applicatoin
   const [user, setUser] = useState(null);
-
+  
+  // Enable dynamic resize of main based on fixed position nav
   const headerRef = useRef(null);
-  useEffect(_ => {
+  const [headerOffset, setHeaderOffset] = useState('0px');
+  function handleResize() {
     setHeaderOffset(headerRef.current.clientHeight);
+  }
+
+  // add function to listen to resize event
+  useEffect(_ => {
+    window.addEventListener('resize', handleResize);
+    setHeaderOffset(headerRef.current.clientHeight)
+
+    return _ => window.removeEventListener('resize', handleResize);
   }, [user]);
 
   return (
-    <>
-      <AuthContext.Provider value={user}>
-        <Header headerRef={headerRef} expand='lg' color='light' />
-        <main style={{ marginTop: headerOffset }}>
-          <Outlet context={{ user, setUser }} />
-        </main>
-      </AuthContext.Provider>
-    </>
+    <AuthContext.Provider value={user}>
+      <Header headerRef={headerRef} expand='lg' color='light' />
+      <main style={{ marginTop: headerOffset }}>
+        <Outlet context={{ user, setUser }} />
+      </main>
+    </AuthContext.Provider>
   );
 }
 
